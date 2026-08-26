96370abc84c37ce273748b65d486f32c6c43db22
anas <anas>
2026-08-27 00:21:27 +0300

    edit profile link

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -1,3 +1,3 @@
 export const GITHUB_URL = 'https://github.com/Anas-Bassit';
 export const LINKEDIN_URL =
-  'https://www.linkedin.com/in/anas-bassit-7548132a7/';
+  'https://www.linkedin.com/in/anas-bassit-8293732b9/';
